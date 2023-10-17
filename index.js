19f33f62ecb6238d64a7c55ed7c9b498e25d7069
shaharul siyam <shaharulsiyam0273@.com>
2023-10-17 19:59:45 +0600

    EpicTechSpot backend setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the root path');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// run().catch(console.dir);
